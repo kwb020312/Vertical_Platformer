1acbbe502f1d7778a234be6f45aa9ac3396c8333
김우빈 <kwb020312@naver.com>
2023-09-16 00:42:43 +0900

    Feat: Project Setup

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-c.fillRect(10, 10, 50, 50);
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
