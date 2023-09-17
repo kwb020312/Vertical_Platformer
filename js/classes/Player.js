79681a5cffcf0b4e634518017cac94e30865b038
김우빈 <kwb020312@naver.com>
2023-09-17 19:49:28 +0900

    Feat: Apply Hitbox

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -8,17 +8,42 @@ class Player extends Sprite {
     };
 
     this.collisionBlocks = collisionBlocks;
+    this.hitbox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      width: 10,
+      height: 10,
+    };
   }
 
   update() {
     this.updateFrames();
-    c.fillStyle = "rgba(0, 255, 0, 0.3)";
+    this.updateHitbox();
+
+    // 이미지 그리기
+    c.fillStyle = "rgba(0, 255, 0, 0.2)";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    c.fillStyle = "rgba(255, 0, 0, 0.2)";
+    c.fillRect(
+      this.hitbox.position.x,
+      this.hitbox.position.y,
+      this.hitbox.width,
+      this.hitbox.height
+    );
+
     this.draw();
 
     this.position.x += this.velocity.x;
+
+    this.updateHitbox();
+
     this.checkForHorizontalCoollisions();
     this.applyGravity();
+    this.updateHitbox();
+
     this.checkForVerticalCoollisions();
   }
 
@@ -27,26 +52,44 @@ class Player extends Sprite {
     this.velocity.y += gravity;
   }
 
+  updateHitbox() {
+    this.hitbox = {
+      position: {
+        x: this.position.x + 35,
+        y: this.position.y + 26,
+      },
+      width: 14,
+      height: 27,
+    };
+  }
+
   checkForVerticalCoollisions() {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
       const collisionBlock = this.collisionBlocks[i];
 
       if (
         collision({
-          object1: this,
+          object1: this.hitbox,
           object2: collisionBlock,
         })
       ) {
         if (this.velocity.y > 0) {
           this.velocity.y = 0;
-          this.position.y = collisionBlock.position.y - this.height - 0.01;
+
+          const offset =
+            this.hitbox.position.y - this.position.y + this.hitbox.height;
+
+          this.position.y = collisionBlock.position.y - offset - 0.01;
           break;
         }
 
         if (this.velocity.y < 0) {
           this.velocity.y = 0;
+
+          const offset = this.hitbox.position.y - this.position.y;
+
           this.position.y =
-            collisionBlock.position.y + collisionBlock.height + 0.01;
+            collisionBlock.position.y + collisionBlock.height - offset + 0.01;
           break;
         }
       }
@@ -59,20 +102,27 @@ class Player extends Sprite {
 
       if (
         collision({
-          object1: this,
+          object1: this.hitbox,
           object2: collisionBlock,
         })
       ) {
         if (this.velocity.x > 0) {
           this.velocity.x = 0;
-          this.position.x = collisionBlock.position.x - this.width - 0.01;
+
+          const offset =
+            this.hitbox.position.x - this.position.x + this.hitbox.width;
+
+          this.position.x = collisionBlock.position.x - offset - 0.01;
           break;
         }
 
         if (this.velocity.x < 0) {
           this.velocity.x = 0;
+
+          const offset = this.hitbox.position.x - this.position.x;
+
           this.position.x =
-            collisionBlock.position.x + collisionBlock.width + 0.01;
+            collisionBlock.position.x + collisionBlock.width - offset + 0.01;
           break;
         }
       }
